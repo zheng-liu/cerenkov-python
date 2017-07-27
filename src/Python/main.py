a5101d8df6c979da3cf4f1513c5672fe94a6c864
zheng-liu <zayne.liu@hotmail.com>
2017-07-26 17:46:23 -0700

    fold_generate

--- src/Python/main.py
+++ src/Python/main.py
@@ -8,8 +8,23 @@ data_osu["coord"] = coord.coord
 data_osu.index.name = "rsid"
 data_osu.set_index("coord", append=True, inplace=True)
 
+# start_time = time.time()
+# fold = locus_sampling(data_osu, n_rep=200, n_fold=5, cutoff_bp=50000, slope_allowed=0.5, seed=1337)
+# end_time = time.time()
+# print fold
+# print "time=", end_time - start_time
+
+# start_time = time.time()
+# fold = snp_sampling(data_osu, n_rep=200, n_fold=5)
+# end_time = time.time()
+# print fold
+# print "time=", end_time - start_time
+
 start_time = time.time()
-fold = locus_sampling(data_osu, n_rep=200, n_fold=5, cutoff_bp=50000, slope_allowed=0.5, seed=1337)
+kwargs={}
+kwargs["cutoff_bp"] = 50000
+kwargs["slope_allowed"] = 0.5
+kwargs["seed"] = 1337
+fold = fold_generate(data_osu, n_rep=200, n_fold=5, fold_assign_method="SNP", **kwargs)
 end_time = time.time()
-print fold
 print "time=", end_time - start_time
